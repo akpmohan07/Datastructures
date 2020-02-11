f0ad49faa864535d681f4e91f81c6540e34081be
akpmohan07 <akpmohan07@gmail.com>
2020-02-11 23:30:41 +0530

    bst insertion

--- bst.java
+++ bst.java
@@ -17,16 +17,44 @@ class tree
         this.root=null;
     }
     
-    void insert(Node root,int data)
+    Node insert(Node root,int data)
     {
         
         if(root==null)
+        {
         Node nn = new Node(data);
         root = nn;
+        return root;
+        }
+        else if(root.data>data)
+        root.lchild = insert(root.lchild,data);
         else if(root.data<data)
-        lchild = 
+        root.rchild = insert(root.rchild,data);
+        return root;
+    }
+    void inorderRec(Node root) { 
+        if (root != null) { 
+            inorderRec(root.lchild); 
+            System.out.println(root.data); 
+            inorderRec(root.rchild); 
+        } 
+    } 
+    
+}
+class main
+{
+    public static void main(String args[])
+    {
+        tree t = new tree();
+        t.root = t.insert(t.root, 2);
+        t.insert(t.root, 3);
+        t.insert(t.root, 4);
+        t.insert(t.root, 15);
+        t.insert(t.root, 6);
+        t.insert(t.root, 7);
+        t.insert(t.root, 8);
 
-
+        t.inorderRec(t.root);
 
 
     }
